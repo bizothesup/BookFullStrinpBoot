df0a88ac0469644dddaf66090fc70aa2123b782c
Mbaye Babacar SARR <mbayesarr.baba@gmail.com>
2022-04-29 19:06:52 +0200

    Initial commit

---
+++ chapitre01/spring-boot01/src/test/java/com/mbs/springboot01/SpringBoot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbs.springboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
